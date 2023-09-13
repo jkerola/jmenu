3ea962f67a3fa679defdce93e2a58c029c9a2ba4
Janne Kerola <janne.kerola@oulu.fi>
2023-09-13 13:49:13 +0300

    fix version check for ci

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,16 @@
 from jmenu.main import get_version, main
-from importlib.metadata import version
+from importlib.metadata import version, PackageNotFoundError
 from unittest.mock import patch
 from conftest import mock_fetch_restaurant
 import pytest
 
 
 def test_version_system():
-    assert get_version() == version("jmenu")
+    try:
+        comp = version("jmenu")
+    except PackageNotFoundError:
+        comp = "development build"
+    assert get_version() == comp
 
 
 @patch("jmenu.api.requests.get", side_effect=mock_fetch_restaurant)
