6209c01b131b2a2043b14163c37e106aabf3d3d2
Janne Kerola <janne.kerola@oulu.fi>
2023-09-13 12:30:56 +0300

    feat: restructure package as library (#27)
    * restructure app for library use
    
    * add error tracking, exit codes
    
    * add unit tests
    
    * add unit test workflow
    
    * revamp versioning system

---
+++ src/jmenu/api.py
@@ -0,0 +1,98 @@
+"""api.py
+
+Contains functions used to wrangle the JAMIX API.
+This file can be imported and exposes the following functions:
+
+    * fetch_restaurant
+    * parse_items_from_response
+    * get_menu_items
+
+The following constants are also exposed:
+    
+    * API_URL
+"""
+
+
+import requests
+from datetime import datetime
+from .classes import Restaurant, MenuItem, SKIPPED_ITEMS
+
+API_URL = "https://fi.jamix.cloud/apps/menuservice/rest/haku/menu"
+
+
+def fetch_restaurant(rest: Restaurant, fetch_date: datetime) -> list[dict]:
+    """Return the JSON response containing all menu information for [Restaurant]
+
+    Parameters
+    ----------
+    rest : Restaurant
+        dataclass containing relevant restaurant information, see classes.RESTAURANTS
+    fetch_date : datetime
+        datetime object used to fetch the date specified menu
+
+    Returns
+    -------
+    list[dict]
+        parsed response json
+    """
+    response = requests.get(
+        f"{API_URL}/{rest.client_id}/{rest.kitchen_id}?lang=fi&date={fetch_date.strftime('%Y%m%d')}",
+        timeout=5,
+    )
+    return response.json()
+
+
+def get_menu_items(rest: Restaurant, fetch_date: datetime) -> list[MenuItem]:
+    """Returns a list of restaurant [MenuItems]
+
+    Parameters
+    ----------
+    rest : Restaurant
+        dataclass containing relevant restaurant information, see classes.RESTAURANTS
+    fetch_date : datetime
+        datetime object used to fetch the date specified menu
+
+    Returns
+    -------
+    list[MenuItem]
+        list of restaurant menu items, see classes.MenuItem
+    """
+    data = fetch_restaurant(rest, fetch_date)
+    items = parse_items_from_response(data, rest.relevant_menus)
+    return items
+
+
+def parse_items_from_response(
+    data: list[dict], relevant_menus: list[str] = []
+) -> list[MenuItem]:
+    """Returns a list of [MenuItems] parsed from JSON data
+
+    Parameters
+    ----------
+    data : list[dict]
+        parsed JSON response from the jamix API, see api._fetch_restaurant
+    relevant_menus : list[str]
+        list of menu names to consider when parsing
+        defaults to empty list
+
+    Returns
+    -------
+    list[MenuItem]
+        list of restaurant menu items, see classes.MenuItem"""
+    menus = []
+    for kitchen in data:
+        for m_type in kitchen["menuTypes"]:
+            if len(relevant_menus) == 0 or m_type["menuTypeName"] in relevant_menus:
+                menus.extend(m_type["menus"])
+    if len(menus) == 0:
+        return []
+    items = []
+    for menu in menus:
+        day = menu["days"][0]
+        mealopts = day["mealoptions"]
+        sorted(mealopts, key=lambda x: x["orderNumber"])
+        for opt in mealopts:
+            for item in opt["menuItems"]:
+                if item["name"] not in SKIPPED_ITEMS:
+                    items.append(MenuItem(item["name"], item["diets"]))
+    return items
--- src/jmenu/main.py
+++ src/jmenu/main.py
@@ -1,34 +1,61 @@
-from .version import VERSION
-from .restaurants import (
-    RESTAURANTS,
-    MARKINGS,
-    API_URL,
-    SKIPPED_ITEMS,
-    Restaurant,
-)
+"""main.py
+
+This file contains the logic for executing jmenu from the command line.
+This file can be imported and exposes the following functions:
+    * main
+    * get_version
+"""
+
+from .classes import RESTAURANTS, MARKERS, MenuItem
+from .api import get_menu_items
 from datetime import datetime, timedelta
-import requests
 import argparse
 from time import time
+from sys import exit
+from importlib.metadata import version, PackageNotFoundError
 
 
-class ArgsNamespace:
+class _ArgsNamespace:
+    """Dataclass for managing command line arguments
+
+    Attributes
+    ---
+    explain : bool
+        print allergen marker info
+    allergens : list[str]
+        highlight the provided allergen markers
+    tomorrow: bool
+        fetch the menus for tomorrow
+    """
+
     explain: bool
     allergens: list[str]
     tomorrow: bool
 
 
 def main():
-    args = get_args()
+    """Fetch and print restaurant menus
+
+    Returns
+    ---
+    success : int
+        returns 1 if any errors were encountered
+        returns 0 otherwise
+    """
+    args = _get_args()
     if args.explain:
-        print_explanations()
+        _print_explanations()
         exit(0)
     start = time()
-    print_menu(args)
+    errors = _print_menu(args)
     print("Process took {:.2f} seconds.".format(time() - start))
+    if errors:
+        exit(1)
+    else:
+        exit(0)
 
 
-def get_args():
+def _get_args():
     parser = argparse.ArgumentParser(
         description="Display University of Oulu restaurant menus for the day"
     )
@@ -37,7 +64,7 @@ def get_args():
         "--version",
         action="version",
         help="display version information",
-        version=VERSION,
+        version=get_version(),
     )
     parser.add_argument(
         "-e",
@@ -64,88 +91,63 @@ def get_args():
         nargs="+",
         help='list of allergens, for ex. "g veg"',
     )
-    return parser.parse_args(namespace=ArgsNamespace())
+    return parser.parse_args(namespace=_ArgsNamespace())
 
 
-def get_restaurant_menu_items(rest: Restaurant, fetch_date: datetime) -> list[dict]:
-    response = requests.get(
-        f"{API_URL}/{rest.client_id}/{rest.kitchen_id}?lang=fi&date={fetch_date.strftime('%Y%m%d')}"
-    )
-    data = response.json()
-    menus = get_menus(data, rest)
-    if len(menus) == 0:
-        return []
-    items = get_menu_items(menus, rest)
-    return items
-
-
-def get_menu_items(menus: dict, rest: Restaurant) -> list[dict]:
-    items = []
-    for menu in menus:
-        day = menu["days"][0]
-        mealopts = day["mealoptions"]
-        sorted(mealopts, key=lambda x: x["orderNumber"])
-        for opt in mealopts:
-            for item in opt["menuItems"]:
-                if item["name"] not in SKIPPED_ITEMS:
-                    items.append(item)
-    return items
-
-
-def get_menus(data: dict, rest: Restaurant) -> list[dict]:
-    menus = []
-    for kitchen in data:
-        for m_type in kitchen["menuTypes"]:
-            if m_type["menuTypeName"] in rest.relevant_menus:
-                menus.extend(m_type["menus"])
-    return menus
-
-
-def print_menu(args: ArgsNamespace):
+def _print_menu(args: _ArgsNamespace):
+    errors = []
     fetch_date = datetime.now()
     if args.tomorrow:
         fetch_date += timedelta(days=1)
+
     allergens = []
     if args.allergens:
         allergens = [x.lower() for x in args.allergens]
 
-    print_header(fetch_date)
+    _print_header(fetch_date)
     for res in RESTAURANTS:
         try:
-            items = get_restaurant_menu_items(res, fetch_date)
-            sorted(items, key=lambda x: x["orderNumber"])
+            items = get_menu_items(res, fetch_date)
             if len(items) == 0:
                 print(res.name.ljust(8), "--")
             else:
                 print(res.name)
                 if not allergens:
-                    for item in items:
-                        print("\t", item["name"], item["diets"])
+                    print(*[f"\t {item.name} {item.diets}" for item in items], sep="\n")
                 else:
-                    print_highlight(items, allergens)
+                    _print_highlight(items, allergens)
 
-        except Exception:
+        except Exception as e:
+            errors.append(e)
             print("Couldn't fetch menu for", res.name)
+    return errors
 
 
-def print_explanations():
-    for mark in MARKINGS:
+def _print_explanations():
+    for mark in MARKERS:
         print(mark.letters, "\t", mark.explanation)
 
 
-def print_highlight(items: list[str], allergens: list[str]):
+def _print_highlight(items: list[MenuItem], allergens: list[str]):
     for item in items:
-        diets = [x.strip().lower() for x in item["diets"].split(",")]
+        diets = [diets.strip().lower() for diets in item.diets.split(",")]
         if all(marker in diets for marker in allergens):
-            print("\033[92m", "\t", item["name"], item["diets"], "\033[0m")
+            print("\033[92m", "\t", item.name, item.diets, "\033[0m")
         else:
-            print("\t", item["name"], item["diets"])
+            print("\t", item.name, item.diets)
 
 
-def print_header(fetch_date: datetime):
+def _print_header(fetch_date: datetime):
     print("-" * 79)
     print("Menu for", fetch_date.strftime("%d.%m"))
     print("-" * 79)
+
+
+def get_version() -> str:
+    try:
+        return version("jmenu")
+    except PackageNotFoundError:
+        return "development build"
 
 
 if __name__ == "__main__":
